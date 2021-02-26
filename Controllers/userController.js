e1fa02965605965dc0b1e0faf6574442f9dcf265
Chiragbhai Jitendrakumar Patel <chiragbhaip@cybage.com>
2021-02-26 12:08:14 +0530

    added reset password api

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const userSchema = require("../Models/userModel");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const otpGenerator = require("otp-generator");
+const sendEmail = require("../Helpers/emailSend");
+
+const userDataCollection = mongoose.model("user", userSchema, "users");
 
 // authentication of the user
 exports.authenticate = async (request, response, next) => {
@@ -14,80 +18,76 @@ exports.authenticate = async (request, response, next) => {
 
   // generates the jwt token
   if (user && bcrypt.compareSync(password, user.password)) {
-    const token = jwt.sign( { userId: user._id, role: user.role}, process.env.JWT_SECRET, { expiresIn: "7d" });
+    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "7d" });
     console.log(token);
-    return response.status(200).json({token:token,firstName:user.firstName})
-    
-  }else if(!user){
-    response.status(404).json({message:"User not found"}); 
-  }else{
-    response.status(400).json({message:"User & Password is not match"}); 
+    return response.status(200).json({ token: token, firstName: user.firstName })
+
+  } else if (!user) {
+    response.status(404).json({ message: "User not found" });
+  } else {
+    response.status(400).json({ message: "User & Password is not match" });
   }
 };
 
-exports.postUser = (request, response, next) => {
+exports.postUser = async (request, response, next) => {
   console.log("In Post User Controller");
-
-  const userDataCollection = mongoose.model("user", userSchema, "users");
-
-  if (request.body.role == "NU" || request.body.role == "RO") {
-    const normalUser = new userDataCollection({
-      firstName: request.body.firstName,
-      lastName: request.body.lastName,
-      email: request.body.email,
-      password: bcrypt.hashSync(request.body.password,10),
-      birthDate: request.body.birthDate,
-      gender: request.body.gender,
-      mobileNumber: request.body.mobileNumber,
-      role: request.body.role,
-    });
-
-    normalUser
-      .save()
-      .then((user) => {
-        response.send(user).status(201).json({message:"User Created Successfully"});
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        next(error);
-      });
-  } 
-  
-  else if (request.body.role == "DE") {
-
-    const deliveryExecutiveUser = new userDataCollection({
-      firstName: request.body.firstName,
-      lastName: request.body.lastName,
-      email: request.body.email,
-      password: bcrypt.hashSync(request.body.password,10),
-      birthDate: request.body.birthDate,
-      gender: request.body.gender,
-      mobileNumber: request.body.mobileNumber,
-      role: request.body.role,
-      deliveryExecutive: request.body.deliveryExecutive,
-    });
-
-    deliveryExecutiveUser
-      .save()
-      .then((user) => {
-        response.send(user).status(201).json({message:"Delivery Executive Created Successfully"});
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-        next(error);
-      });
+  const normalUser = new userDataCollection({
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    email: request.body.email,
+    password: bcrypt.hashSync(request.body.password, 10),
+    birthDate: request.body.birthDate,
+    gender: request.body.gender,
+    mobileNumber: request.body.mobileNumber,
+    role: request.body.role,
+  });
+  if (request.body.role == "DE") {
+    normalUser.deliveryExecutive = request.body.deliveryExecutive;
   }
+  normalUser
+    .save((err, res) => {
+      if (err) {
+        //console.log(err);
+        response.status(200).json({ message: "User is already exists with this email!!" });
+      }
+      //console.log(res);
+      response.status(201).json({ message: "User Created Successfully" });
+    })
 };
 
-// For login the user
-// exports.loggedInUser = async (request, response, next) => {
-//   const userDataCollection = mongoose.model("user", userSchema, "users");
-//   const email = request.body.email;
-//   const password = request.body.password;
-//   const userObj = await userDataCollection.find({
-//     email: email,
-//     password: password,
-//   });
-//   console.log(userObj);
-//   response.send(userObj);
-// };
+exports.sendOtpForForgotPassword = async(request,response,next)=>{
+  
+  const email=request.body.email;
+  var userData=await userDataCollection.findOne({email:email});
+  if(userData){
+    const generatedForgotPasswordOtp = otpGenerator.generate(6, {
+      upperCase: false,
+      specialChars: false,
+      alphabets: false,
+    });
+    const html = generatedForgotPasswordOtp;
+    sendEmail.sendMails([email], "Foodizz Reset Password OTp", html);
+    response.status(200).json({forgotPasswordOtp:generatedForgotPasswordOtp,message:"Check you email inbox. OTP Send Succeccfully!!!"});  
+  }else{
+    response.status(200).json({message:"User is not exist with this email"});  
+  }
+}
+
+exports.resetPassword = async(request,response,next)=>{
+  const email=request.body.email;
+  const newPassword=request.body.newPassword;
+  var userData=await userDataCollection.findOne({email:email});
+  if(userData){
+    userData=await userData.resetPassword(bcrypt.hashSync(newPassword, 10));
+    // console.log("bcrypt : ",bcrypt.compareSync(newPassword, userData.password))
+    // console.log("useData :",userData);
+
+    if(userData){
+     response.status(200).json({message:"Your Password is reset successfully"});
+    }else{
+      response.status(200).json({message:"Your Password is not reset successfully"});
+    }
+  }else{
+    response.status(200).json({message:"Your Password is not reset successfully"});
+  }
+}
